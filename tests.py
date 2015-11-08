dc77fa6a54afdb651141726cbecae6cb74c9eec1
Andrew Marks <ajmarks@gmail.com>
2015-11-08 10:05:50 -0500

    Fix tests.

--- tests.py
+++ tests.py
@@ -1,5 +1,5 @@
 import unittest
 
 if __name__ == '__main__':
-    unittest.TestLoader.loadTestsFromModule('tests')
+    unittest.TestLoader.loadTestsFromModule('pdf_parser.tests')
     unittest.main()
