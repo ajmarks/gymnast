e8d506fd9447df6095614cd15e2e649415193153
Andrew Marks <amarks@gladebrookcapital.com>
2015-11-17 13:28:35 -0500

    Missed this in the last one

--- pdf_parser/pdf_operation/pdf_operation.py
+++ pdf_parser/pdf_operation/pdf_operation.py
@@ -2,21 +2,20 @@
 PDF Operations base class
 """
 
+import inspect
 import six
 import warnings
-
 from ..exc  import PdfOpWarning
-from ..misc import get_subclasses, classproperty, ensure_str, MetaGettable
+from ..misc import classproperty, ensure_str, MetaGettable
 
 __all__ =['PdfOperation']
 
 
 @six.add_metaclass(MetaGettable)
 class PdfOperation(object):
-    """PDF content stream operations."""
-    opcode = None
-    optype = None
-
+    """PDF content stream operations dispatcher.  Content stream operations are
+    registered by calling PdfOperation.register(), passing the opcode, the type
+    of operation, and the function that does it."""
     # Operation type constants (see Reference p. 196)
     # These mostly exist to make things easier in PdfRenderer methods
     NOP                    =     0
@@ -37,58 +36,53 @@ class PdfOperation(object):
     MARKED_CONTENT         = 16384
     COMPATIBILITY          = 32768
 
-    _opcode = None
-
-    @staticmethod
-    def do_opcode(renderer, *operands):
-        raise NotImplementedError
-
-    # Don't worry about anythign down here.  Just fill in the opcode, implement
-    # do_opcode(), have a beer, and relax.
-    __opcodes = None
-    @classproperty
-    def opcodes(cls):
-        if PdfOperation.__opcodes is None:
-            PdfOperation.__init_opcodes()
-        return PdfOperation.__opcodes
-
-    def __init__(self, *operands):
-        self._operands = operands
-
-    def __call__(self, renderer):
-        return self.do_opcode(renderer, *self._operands)
+    _opcodes = {}
+    _nops    = {} # As we spawn new NOP classes, we'll cache them here
+    @classmethod
+    def register(cls, opcode, optype, opfunc):
+        """Register a new PDF operation.  Arguments are the opcode, the type of
+        operation, and the function that actually does the operation."""
+        opcode = ensure_str(opcode)
+        if not callable(opfunc):
+            raise TypeError('opfunc must be callable')
+        args = inspect.getfullargspec(opfunc)
+        if not (args.varargs or args.args):
+            raise ValueError('opfunc must take at least one positional argument')
+        opcode = ensure_str(opcode)
+        cls._opcodes[opcode] = new_opcode(opcode, optype, opfunc)
 
     @classmethod
     def __getitem__(cls, operator):
         operator = ensure_str(operator)
         try:
-            return cls.opcodes[operator]
+            return cls._opcodes[operator]
         except KeyError:
-            return type(operator, (PdfNOP,), {'_opcode':operator})
+            return cls._nop(operator)
+    @classmethod
+    def _nop(cls, operator):
+        """Get the corresponding NOP operation, creating it if necessary."""
+        warnings.warn("Opcode '{}' not implemented. NOP".format(operator))
+        try:
+            return cls._nops[operator]
+        except KeyError:
+            tpe = new_opcode(operator, 0, lambda *x: None)
+            cls._nops[operator] = tpe
+            return tpe
 
-    @staticmethod
-    def __init_opcodes():
-        from . import operations
-        opcodes = {ensure_str(o.opcode): o
-                   for o in get_subclasses(PdfOperation)
-                     if o.opcode}
-        PdfOperation.__opcodes = opcodes
+class PdfBaseOp(object):
+    """Base class for all pdf operations"""
+    opcode = None
+    optype = None
+    opfunc = lambda x: None
+    def __init__(self, *operands):
+        self._operands = operands
+    def __call__(self, renderer):
+        return (self.opfunc.__func__)(renderer, *self._operands)
     def __str__(self):
         op = self.opcode if self.opcode else self._opcode
         return '{}({})'.format(op, ', '.join(str(i) for i in self._operands))
 
-class PdfNOP(PdfOperation):
-    """Dummy opcode that does nothing.  Called when we've not
-    implemented a given opcode"""
-
-    optype = PdfOperation.NOP
-    def __call__(self, renderer):
-        cname = self.__class__.__name__
-        warnings.warn("Opcode '%s' not implemented.  NOP." % cname,
-                      PdfOpWarning)
-        return self.do_opcode(renderer, *self._operands)
-
-    @staticmethod
-    def do_opcode(renderer, *args):
-        #Some logging stuff should go here
-        pass
+def new_opcode(opcode, optype, opfunc):
+    """Create a new PDF operation based on the arguments"""
+    class_data = {'opcode': opcode, 'optype':optype, 'opfunc':opfunc}
+    return type(opcode, (PdfBaseOp, ), class_data)
