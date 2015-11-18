d06f81f5598708a9dedf475fc51102fdd15a6463
Andrew Marks <amarks@gladebrookcapital.com>
2015-11-18 14:05:16 -0500

    Update SimpleRenderer

--- pdf_parser/renderer/simple_renderer.py
+++ pdf_parser/renderer/simple_renderer.py
@@ -8,10 +8,12 @@ from .base_renderer import PdfBaseRenderer
 class PdfSimpleRenderer(PdfBaseRenderer):
     """Simple renderer example that just extracts the text with no processing"""
     def __init__(self, page):
+        """Create a new naive rendered that just collects all of the text"""
         super(PdfSimpleRenderer, self).__init__(page)
         self._text = io.StringIO()
     def _render_text(self, text, new_state):
-        for glyph in text:
-            self._text.write(self.active_font.decode_char(glyph))
+        """Add the text to the buffer"""
+        self._text.write(self.active_font.decode_string(text))
     def _return(self):
+        """Return collected text"""
         return self._text.getvalue()
