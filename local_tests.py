307eb41594779ad77571fe124d22dc16fd8bcf9f
Andrew Marks <ajmarks@gmail.com>
2015-11-22 14:20:44 -0500

    Update local tests file

--- local_tests.py
+++ local_tests.py
@@ -4,16 +4,15 @@ sys.path.insert(1, './')
 import requests
 
 from gymnast            import PdfDocument
-from gymnast            import PdfLineRenderer
+from gymnast            import PdfTextRenderer
 from gymnast.pdf_parser import PdfParser
 
 #fname = 'c:/855.pdf'
 url = 'http://www.census.gov/retail/mrts/www/data/pdf/ec_current.pdf'
 r = requests.get(url)
 pdf = PdfDocument(r.content).parse()
-page = pdf.Pages[4]
-print(page.Fonts['T1_1'].FontDescriptor.CharSet)
-text = PdfLineRenderer(page, True, None).render()
+page = pdf.Pages[1]
+text = PdfTextRenderer(page, True, None).render()
 print(text)
 #fname = 'c:/results.pdf'
 #pdf = PdfDocument(fname)
